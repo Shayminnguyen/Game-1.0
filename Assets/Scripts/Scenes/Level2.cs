15495c7f241ea67e793357cf70d76f2c18e4163b
Rin Shaymin <Rin Shaymin>
2017-02-20 01:47:46 +0700

    BTVN1_Final

--- Assets/Scripts/Scenes/Level2.cs
+++ Assets/Scripts/Scenes/Level2.cs
@@ -6,8 +6,14 @@ using UnityEngine.UI;
 public class Level2 : MonoBehaviour {
     public InputField passwordInput;
     public Text hint;
+    public Text level;
     string CORRECT_ANSWER = "solomon";
     string INCORRECT_ANSWER = "ACCESS DENIED";
+    string LEVEL_MESSAGE = "LEVEL";
+    string LEVEL_INFOR = "2";
+
+    int interval = 2;
+    float nextTime = 0;
 
     public void OnSubmitButtonClicked()
     {
@@ -15,11 +21,23 @@ public class Level2 : MonoBehaviour {
         {
             TKSceneManager.ChangeScene("Level3");
         }
-        else
+       else
         {
             hint.text = INCORRECT_ANSWER;
             hint.color = Color.red;
             passwordInput.text = "";
+        }
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad >= nextTime)
+        {
+            if (level.text.Equals(LEVEL_MESSAGE))
+                level.text = LEVEL_INFOR;
+            else
+                level.text = LEVEL_MESSAGE;
+            nextTime += interval;
         }
     }
 }
--- Assets/Scripts/Scenes/Level3.cs
+++ Assets/Scripts/Scenes/Level3.cs
@@ -3,27 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Level1 : MonoBehaviour {
+public class Level3 : MonoBehaviour {
     public InputField passwordInput;
     public Text hint;
     public Text level;
-    string CORRECT_ANSWER = "kabul";
+    public Image image1;
+    public Image image2;
+    string CORRECT_ANSWER = "evilcat";
     string INCORRECT_ANSWER = "ACCESS DENIED";
     string LEVEL_MESSAGE = "LEVEL";
-    string LEVEL_INFOR = "1";
+    string LEVEL_INFOR = "3";
 
     int interval = 2;
     float nextTime = 0;
 
     public void OnSubmitButtonClicked()
     {
-        if (passwordInput.text.Equals(CORRECT_ANSWER)) {
-            TKSceneManager.ChangeScene("Level2");
-        } else {
+        if (passwordInput.text.Equals(CORRECT_ANSWER))
+        {
+            TKSceneManager.ChangeScene("StartScene");
+        }////************************************************************
+        else
+        {
             hint.text = INCORRECT_ANSWER;
             hint.color = Color.red;
             passwordInput.text = "";
         }
+    }
+
+    public void OnPagesButtonClicked()
+    {
+        image1.enabled = !image1.enabled;
+        image2.enabled = !image2.enabled;
     }
 
     void Update()
@@ -37,5 +48,4 @@ public class Level1 : MonoBehaviour {
             nextTime += interval;
         }
     }
-
 }
